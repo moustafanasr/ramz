4be9b9f52a856b7e97793f5b543ad8e9d08217d2
Moustafa-M-Nasr <moustafanasr126@gmail.com>
2024-12-09 16:06:20 +0200

    Upload-7 9-12-2024 4-06pm Upload-7 9-12-2024 4-06pm Upload-7 9-12-2024 4-06pm

--- js/javascript.js
+++ js/javascript.js
@@ -262,9 +262,10 @@ setInterval(shift, 3000);
 
 const scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
+// If a user hasn't opted in for reduced motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
+  adjustScrollEffect();
 }
 
 function addAnimation() {
@@ -286,6 +287,36 @@ function addAnimation() {
     });
   });
 }
+
+function adjustScrollEffect() {
+  const section = document.querySelector(".section-dimensions");
+  const scrollerRight = section.querySelector('.scroller[data-direction="right"]');
+  const scrollerLeft = section.querySelector('.scroller[data-direction="left"]');
+
+  window.addEventListener("scroll", () => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const scrollY = window.scrollY;
+
+    if (scrollY >= (sectionTop -1000) && scrollY <= (sectionTop +1000) + sectionHeight) {
+      // Calculate the percentage scrolled within the section
+      const progress = (scrollY - sectionTop) / sectionHeight;
+      console.log(progress)
+      // Update transforms based on scroll progress
+      // const translateXLeft = Math.min(0, -10 - 10 * progress); // From -10% to 0%
+
+      // const translateXRight = Math.max(-150, -150 + 140 * progress); // From -150% to -10%
+      const translateXRight = Math.max(-200, -10 - 40 * progress); // From -10% to -200%
+      const translateXLeft = Math.max(-200, -200 + 40 * progress); // From -200% to -10%
+
+      scrollerRight.style.transform = `translateX(${translateXRight}%)`;
+      scrollerLeft.style.transform = `translateX(${translateXLeft}%)`;
+      scrollerRight.style.webkitTransform = `translateX(${translateXRight}%)`;
+      scrollerLeft.style.webkitTransform = `translateX(${translateXLeft}%)`;
+    }
+  });
+}
+
 
 
 
@@ -365,14 +396,11 @@ videocontainers.forEach((div, index) => {
 
   if (img && title && history && paragraph && mainTitle) {
     let itemIndex = index; // Track the current index
-    let translateX = 0; // Current translateX value
-    let lastMouseX = 0; // Last X-coordinate of the mouse
-    let isDragging = false; // Dragging state
 
     // Function to update content based on the current index
     const updateContent = () => {
       const item = content[itemIndex];
-      img.src = item.src;
+      // img.src = item.src;
       title.textContent = item.title;
       history.textContent = item.history;
       paragraph.textContent = item.paragraph;
@@ -381,171 +409,9 @@ videocontainers.forEach((div, index) => {
 
     // Initialize the content for the current div
     updateContent();
-
-    // const serviceMouse = document.querySelector('.section-service .about-left');
-
-    // // Mouse down event
-    // serviceMouse.addEventListener('mousedown', (event) => {
-    //   isDragging = true;
-    //   lastMouseX = event.clientX; // Record starting mouse position
-    //   // Mouse move event
-    //   serviceMouse.addEventListener('mousemove', (event) => {
-    //     if (isDragging) {
-    //       console.log(event.clientX, lastMouseX)
-    //       // console.log()
-    //       // console.log(Math.floor(lastMouseX / 100))
-    //       if (event.clientX > lastMouseX) {
-    //         console.log("good")
-    //         const potentialIndex = index + 1;
-    //         console.log(potentialIndex)
-    //         const currentIndexInArray = potentialIndex * 100
-    //         console.log(currentIndexInArray)
-
-    //         if (potentialIndex >= 0 && potentialIndex < content.length) {
-    //           itemIndex = potentialIndex;
-    //           updateContent(); // Update the displayed content
-    //         }
-    //         div.style.transform = `translateX(${currentIndexInArray}%)`; // Update the element's transform
-    //       }
-    //       else {
-    //         console.log("fail")
-    //         const potentialIndex = index - 1;
-    //         const currentIndexInArray = potentialIndex * 100
-    //         console.log(currentIndexInArray)
-    //         if (potentialIndex >= 0 && potentialIndex < content.length) {
-    //           itemIndex = potentialIndex;
-    //           updateContent();
-    //         }
-    //         div.style.transform = `translateX(-${currentIndexInArray}%)`; // Update the element's transform
-    //       }
-    //     }
-    //   });
-    // });
-    // // Mouse up event
-    // document.addEventListener('mouseup', () => {
-    //   isDragging = false; // Reset dragging state
-    // });
   }
 });
 
 
-
-
-
-let loops = gsap.utils.toArray('.section-service .about-left .div-video').map((line, i) => {
-  const links = line.querySelectorAll(".section-service .about-left .div-video"),
-    tickerDirection = isEven(i),
-    tl = horizontalLoop(links, {
-      repeat: -1,
-      speed: 1 + i * 0.25,
-      draggable: true,
-      reversed: tickerDirection,
-      paddingRight: parseFloat(gsap.getProperty(links[0], "marginRight", "px"))
-    });
-  links.forEach(link => {
-    link.addEventListener("mouseenter", () => gsap.to(tl, { timeScale: 0, overwrite: true }));
-    link.addEventListener("mouseleave", () => gsap.to(tl, { timeScale: 1, overwrite: true }));
-  });
-});
-
-let currentScroll = 0;
-let scrollDirection = 1;
-
-function isEven(n) {
-  return n % 2 == 0;
-}
-
-function horizontalLoop(items, config) {
-  items = gsap.utils.toArray(items);
-  config = config || {};
-  let tl = gsap.timeline({ repeat: config.repeat, paused: config.paused, defaults: { ease: "none" }, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100) }),
-    length = items.length,
-    startX = items[0].offsetLeft,
-    times = [],
-    widths = [],
-    xPercents = [],
-    curIndex = 0,
-    pixelsPerSecond = (config.speed || 1) * 100,
-    snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-    populateWidths = () => items.forEach((el, i) => {
-      widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
-      xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / widths[i] * 100 + gsap.getProperty(el, "xPercent"));
-    }),
-    getTotalWidth = () => items[length - 1].offsetLeft + xPercents[length - 1] / 100 * widths[length - 1] - startX + items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") + (parseFloat(config.paddingRight) || 0),
-    totalWidth, curX, distanceToStart, distanceToLoop, item, i;
-  populateWidths();
-  gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-    xPercent: i => xPercents[i]
-  });
-  gsap.set(items, { x: 0 });
-  totalWidth = getTotalWidth();
-  for (i = 0; i < length; i++) {
-    item = items[i];
-    curX = xPercents[i] / 100 * widths[i];
-    distanceToStart = item.offsetLeft + curX - startX;
-    distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-    tl.to(item, { xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond }, 0)
-      .fromTo(item, { xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100) }, { xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false }, distanceToLoop / pixelsPerSecond)
-      .add("label" + i, distanceToStart / pixelsPerSecond);
-    times[i] = distanceToStart / pixelsPerSecond;
-  }
-  function toIndex(index, vars) {
-    vars = vars || {};
-    (Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
-    let newIndex = gsap.utils.wrap(0, length, index),
-      time = times[newIndex];
-    if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
-      vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
-      time += tl.duration() * (index > curIndex ? 1 : -1);
-    }
-    curIndex = newIndex;
-    vars.overwrite = true;
-    return tl.tweenTo(time, vars);
-  }
-  tl.next = vars => toIndex(curIndex + 1, vars);
-  tl.previous = vars => toIndex(curIndex - 1, vars);
-  tl.current = () => curIndex;
-  tl.toIndex = (index, vars) => toIndex(index, vars);
-  tl.updateIndex = () => curIndex = Math.round(tl.progress() * (items.length - 1));
-  tl.times = times;
-  tl.progress(1, true).progress(0, true); // pre-render for performance
-  if (config.reversed) {
-    tl.vars.onReverseComplete();
-    tl.reverse();
-  }
-  if (config.draggable && typeof (Draggable) === "function") {
-    let proxy = document.createElement("div"),
-      wrap = gsap.utils.wrap(0, 1),
-      ratio, startProgress, draggable, dragSnap, roundFactor,
-      align = () => tl.progress(wrap(startProgress + (draggable.startX - draggable.x) * ratio)),
-      syncIndex = () => tl.updateIndex();
-    typeof (InertiaPlugin) === "undefined" && console.warn("InertiaPlugin required for momentum-based scrolling and snapping. https://greensock.com/club");
-    draggable = Draggable.create(proxy, {
-      trigger: items[0].parentNode,
-      type: "x",
-      onPress() {
-        startProgress = tl.progress();
-        tl.progress(0);
-        populateWidths();
-        totalWidth = getTotalWidth();
-        ratio = 1 / totalWidth;
-        dragSnap = totalWidth / items.length;
-        roundFactor = Math.pow(10, ((dragSnap + "").split(".")[1] || "").length);
-        tl.progress(startProgress);
-      },
-      onDrag: align,
-      onThrowUpdate: align,
-      inertia: true,
-      snap: value => {
-        let n = Math.round(parseFloat(value) / dragSnap) * dragSnap * roundFactor;
-        return (n - n % 1) / roundFactor;
-      },
-      onRelease: syncIndex,
-      onThrowComplete: () => gsap.set(proxy, { x: 0 }) && syncIndex()
-    })[0];
-  }
-
-  return tl;
-}
 
 
